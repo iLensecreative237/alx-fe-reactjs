a5515c64fe47913773c0c57022d08bdd55a86b57
ilensecreative237 <chuiya2740@gmail.com>
2025-07-23 17:20:04 +0100

    task 3 Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1px', backgroundColor: '#eee', marginTop: '1rem' }}>
+      <p style={{ color: '#990099' }}>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
