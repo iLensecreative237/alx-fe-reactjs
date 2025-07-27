7a917ef468f01e962db84ee1cf9cad3d350dcc92
ilensecreative237 <chuiya2740@gmail.com>
2025-07-27 21:27:56 +0100

    Corretion to task

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -10,12 +10,26 @@ export const useRecipeStore = create((set, get) => ({
   addRecipe: (newRecipe) => {
     const updatedRecipes = [...get().recipes, newRecipe];
     set({ recipes: updatedRecipes });
-    get().filterRecipes(); // Update filtered recipes as well
+    get().filterRecipes();
+  },
+
+  updateRecipe: (updatedRecipe) => {
+    const updatedRecipes = get().recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    );
+    set({ recipes: updatedRecipes });
+    get().filterRecipes();
+  },
+
+  deleteRecipe: (id) => {
+    const updatedRecipes = get().recipes.filter(recipe => recipe.id !== id);
+    set({ recipes: updatedRecipes });
+    get().filterRecipes();
   },
 
   setSearchTerm: (term) => {
     set({ searchTerm: term });
-    get().filterRecipes(); // Trigger filtering when search term changes
+    get().filterRecipes();
   },
 
   filterRecipes: () => {
