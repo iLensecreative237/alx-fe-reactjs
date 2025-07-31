449e6b989776569946a54514a28bb0a053ef893c
ilensecreative237 <chuiya2740@gmail.com>
2025-07-31 17:48:37 +0100

    correction to my search.jsx file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -37,7 +37,7 @@ const Search = () => {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can’t find the user</p>}
+      {error && <p>Looks like we cant find the user</p>}
 
       {user && (
         <div>
