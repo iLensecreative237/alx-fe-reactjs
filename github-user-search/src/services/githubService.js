036c2afd61a0383722808e8f8517421cb4091bef
ilensecreative237 <chuiya2740@gmail.com>
2025-08-04 17:49:44 +0100

    Understanding task 3 better

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,7 @@
 // src/services/githubService.js
 import axios from "axios";
-
+console.log("process.env")
+console.log(import.meta.env)
 export const fetchAdvancedUserData = async (username, location, minRepos) => {
   const queryParts = [];
 
