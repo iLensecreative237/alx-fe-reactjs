d303cc4bccf13341ff7d3fd821fd7aa5c167acfe
ilensecreative237 <chuiya2740@gmail.com>
2025-08-08 15:37:40 +0100

    task 3 - Adding Advanced Interactivity and Transitions with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className= "bg-gray-100 p-2 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className= "bg-gray-100 p-2 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="./src/assets/download.jpg"
         alt="User"
-        className= "w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
+        className= "w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">Chu Iya</h1>
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300">Chu Iya</h1>
       <p className="text-sm md:text-base text-gray-600 text-center">
         Operations Mangager at Tranzak Sarl.
       </p>
@@ -18,7 +18,7 @@ function UserProfile() {
       />
       <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/linkedin.svg" 
       alt="LinkedIn" 
-      class="h-6 w-6" 
+      class="h-6 w-6 " 
       />
       <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/facebook.svg" 
       alt="Facebook" 
