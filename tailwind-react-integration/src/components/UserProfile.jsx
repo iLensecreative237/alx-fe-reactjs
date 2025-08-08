814e8f23b3e7d37ed50d85704adeab570d10cd2a
ilensecreative237 <chuiya2740@gmail.com>
2025-08-08 14:32:05 +0100

    task 1 - Styling a Provided React Component with Tailwind CSS

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,37 @@
+function UserProfile() {
+  return (
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img
+        src="./src/assets/download.jpg"
+        alt="User"
+        className="w-36 h-36 rounded-full mx-auto"
+      />
+      <h1 className="text-xl text-blue-800 my-4 text-center">Chu Iya</h1>
+      <p className="text-gray-600 text-base text-center">
+        Operations Mangager at Tranzak Sarl.
+      </p>
+
+      <div class="flex justify-center gap-4 mt-4">
+      <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/github.svg" 
+      alt="GitHub" 
+      class="h-6 w-6" 
+      />
+      <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/linkedin.svg" 
+      alt="LinkedIn" 
+      class="h-6 w-6" 
+      />
+      <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/facebook.svg" 
+      alt="Facebook" 
+      class="h-6 w-6" 
+      />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/X_icon_2.svg/2048px-X_icon_2.svg.png" 
+      alt="X" 
+      class="h-6 w-6" 
+      />
+      </div>
+    </div>
+    
+  );
+}
+
+export default UserProfile;
