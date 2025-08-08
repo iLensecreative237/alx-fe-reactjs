38898ba529ff7fb507b8b04eb69e2f994533d31a
ilensecreative237 <chuiya2740@gmail.com>
2025-08-08 17:27:35 +0100

    more styling on icons

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -12,16 +12,16 @@ function UserProfile() {
       </p>
 
       <div class="flex justify-center gap-4 mt-4">
-        <a href="#">
+        <a href="https://github.com/iLensecreative237/alx-fe-reactjs" class="hover:scale-110 transition-transform duration-300 ease-in-out">
             <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/github.svg" alt="GitHub" class="h-6 w-6"  />
         </a>
-        <a href="#">
+        <a href="https://www.linkedin.com/in/chuiya?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" class="hover:scale-110 transition-transform duration-300 ease-in-out">
             <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/linkedin.svg" alt="LinkedIn" class="h-6 w-6 " />
         </a>
-        <a href="#">
+        <a href="https://www.facebook.com/share/191W3ciuSH/?mibextid=wwXIfr" class="hover:scale-110 transition-transform duration-300 ease-in-out">
             <img src="https://cdn.jsdelivr.net/npm/simple-icons@v7/icons/facebook.svg" alt="Facebook" class="h-6 w-6" />
         </a>
-        <a href="#">
+        <a href="#" class="hover:scale-110 transition-transform duration-300 ease-in-out">
            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/X_icon_2.svg/2048px-X_icon_2.svg.png" alt="X" class="h-6 w-6" />
         </a>
       </div>
