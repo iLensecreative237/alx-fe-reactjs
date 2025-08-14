01ea0ee7ebfff90148d4eed67f0a906ddf42158f
ilensecreative237 <chuiya2740@gmail.com>
2025-08-14 19:42:34 +0100

    task 0

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,10 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",              // root index.html
+    "./public/index.html",       // include public HTML
+    "./src/**/*.{js,ts,jsx,tsx}" // all source files
   ],
   theme: {
     extend: {},
